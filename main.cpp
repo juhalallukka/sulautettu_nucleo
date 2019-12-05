8ad4e5d92a0de19c89e62e26ac6804b03a7d46bd
Juha <lallukka.juha@gmail.com>
2019-12-05 11:09:28 +0200

    lisätty keskiarvon laskeminen kousteudelle ja siistitty koodia

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 
 
+
+
 #include <math.h>
 #include "kosteustaulukko.h"
 #include "mbed.h"
@@ -20,40 +22,40 @@ AnalogIn AnalogToDigitalTemp(A0); //
 AnalogIn AnalogToDigitalMoist(A1);
 DigitalOut PowerToMoisture(D2);
 
-float Uin; //käyttöjännite
+double Uin; //käyttöjännite
+
 
 //*******************LÄMPÖTILA MUUTTUJAT**********************************
-float rawDataTemp; // 3.3V = 1.0 raakadatana, viidellä voltilla tarkempi mittaustulos, mutta yli 3.3V:n arvot virheellisiä
-float temperature; // celsius-asteina
+long double rawDataTemp; // 3.3V = 1.0 raakadatana, viidellä voltilla tarkempi mittaustulos, mutta yli 3.3V:n arvot virheellisiä
+double temperature; // celsius-asteina
 
-float UoutTemp; //mitattu jännite, lämpötila
+double UoutTemp; //mitattu jännite, lämpötila
 
 int resistor1; //sarjavastus ohmeina, kokoluokkaa 1 kOhm
-float resistorTemp; //lämpötila-anturin resistanssi ohmeina
+double resistorTemp; //lämpötila-anturin resistanssi ohmeina
 
 
 
 
-
-
-
-//float kerroin; //vaihtelee 118.05 & 97.254 välillä (0 & 50 *C)
-//float potenssi; // vaihtelee -0.143 & -0.185 välillä (0 & 50 *C)
-
-//void KosteusMittaus(); //kaavamuotoa: kosteus = kerroin * Rk ^ potenssi
 
 
 
 //****************KOSTEUS MUUTTUJAT***********************
-float moisture; //kosteus mitä lasketaan
-float rawDataMoist;
-float UoutMoist; //mitattu jännite
+double moistureAvg1; // painotuksella
+double moistureAvg2; //minuutin keskiarvo
+double moistureAvg2Sum;
+double moistureAvg2Array[18];
+int avgCounter;
 
-float resistor2; //sarjavastus ohmeina, kokoluokkaa 50kOhm
-float resistorMoist; //kosteusanturin resistanssi ohmeina
+double moisture; //kosteus mitä lasketaan
+long double rawDataMoist;
+long double UoutMoist; //mitattu jännite
+
+double resistor2; //sarjavastus ohmeina, kokoluokkaa 50kOhm
+long double resistorMoist; //kosteusanturin resistanssi ohmeina
 
 
-float virtualArrayForMoisture[15]; //lasketaan anturin datasheetissa olevan taulukon pohjalta
+double virtualArrayForMoisture[15]; //lasketaan anturin datasheetissa olevan taulukon pohjalta
 
 int columHigher; //sarakkeet
 int columLower;
@@ -63,8 +65,8 @@ int rowLower;
 
 
 
-float lowerResistance;
-float higherResistance;
+double lowerResistance;
+double higherResistance;
 
 int lowerTemp;
 int higherTemp;
@@ -72,29 +74,32 @@ int higherTemp;
 int lowerMoist;
 int higherMoist;
 
-float weightTempLower; //painotuskertoimet
-float weightTempHigher;
+double weightTempLower; //painotuskertoimet
+double weightTempHigher;
 
-float weightMoistureLower;
+double weightMoistureLower;
 
-float virtualRowNumber;
-float virtualWeightResistanceLow;
-float virtualWeightResistanceHigh;
+double virtualRowNumber;
+bool vRNcalculated;
+double virtualWeightResistanceLow;
+double virtualWeightResistanceHigh;
 
 
 
 
 
 //******************KOSTEUS FUNKTIOT************************
-float CalculateWeight(float value, float lowerValue, float higherValue); //laskee painotuskertoimen lowerValue muuttujalle
+double CalculateWeight(double value, double lowerValue, double higherValue); //laskee painotuskertoimen lowerValue muuttujalle
 void CheckColumNumber();
 
 void CalculateResistanceMoist();
 void CalculateVirtualArrayForMoisture();
 void CalculateVirtualRowNumber();
 void CalculateMoisture();
+void CalculateMoistureAverage();
 
 void MeasureMoisture(); //pääfunktio joka nostaa pinnin HIGH tilaan ja ajaa muut tarvittavat funktiot
+
 
 
 
@@ -107,14 +112,20 @@ int main()
 {
     rawDataTemp = 0;
     rawDataMoist = 0;
+    resistorMoist = 0;
+    resistorTemp = 0;
+    UoutMoist = 0;
+    UoutTemp = 0;
+    avgCounter = 0;
 
     Uin = 3.3; //käyttöjännite
+
     resistor1 = 1000; //lämpötilan sarjavastus ohmeina
-    resistor2 = 47000; //kosteuden sarjavastus ohmeina
+    resistor2 = 830000; //kosteuden sarjavastus kohmeina
 
     PowerToMoisture = 0;
 
-
+    pc.printf("kosteusresistorin alustus: %f", resistorMoist);
 
 //timer.start();
 //timer.reset();
@@ -122,7 +133,7 @@ int main()
 
 	while(true)
 	{
-                //rawDataMoist = AnalogToDigitalMoist;
+               //rawDataMoist = AnalogToDigitalMoist;
 
                rawDataTemp = AnalogToDigitalTemp;
                UoutTemp = Uin - rawDataTemp*Uin; //muutetaan volteiksi. pelkkä lampotilaRaaka*Uin, jos sarvavastus on plusnavan puolella ja lämpötilasensori miinus puolella.
@@ -130,11 +141,14 @@ int main()
                resistorTemp = resistor1/(Uin/UoutTemp -1);
 
                temperature = 0.1291 * resistorTemp -107.99; // tämä vaikutti tarkimmalta pikavertailulla kahden muun mittarin kanssa
-               //temperature = -4*(10^(-5))* resistorTemp^2 + 0.2125 * resistorTemp - 146.78;
+               //temperature = -4*(10^(-5))* resistorTemp^2 + 0.2125 * resistorTemp - 146.78; // tämä ei toimi
                 //temperature = (-0.00004) * (resistorTemp * resistorTemp) + (0.2125 * resistorTemp) - 146.78;
 
 
                MeasureMoisture();
+
+
+
 
                //Tulosteet
                //pc.printf("%f ", rawDataTemp);
@@ -149,6 +163,12 @@ int main()
                pc.printf("\n");
 
                pc.printf("moisture: %f ", moisture);
+               pc.printf("\n");
+
+               pc.printf("moistureAvg: %f ", moistureAvg1);
+               pc.printf("\n");
+
+               pc.printf("moistureAvg: %f ", moistureAvg2);
                pc.printf("\n");
 
                //rivin vaihto
@@ -171,7 +191,7 @@ int main()
 void MeasureMoisture()
 {
     PowerToMoisture = 1;
-    wait(0.01);
+    wait(0.001);
 
     rawDataMoist = AnalogToDigitalMoist;
 
@@ -182,6 +202,8 @@ void MeasureMoisture()
     CalculateVirtualArrayForMoisture();
     CalculateVirtualRowNumber();
     CalculateMoisture();
+    CalculateMoistureAverage();
+
 
 
 }
@@ -191,18 +213,24 @@ void MeasureMoisture()
 
 
 //laskee painotuskertoimen alemmalle arvolle
-float CalculateWeight(float value, float lowerValue, float higherValue)
+double CalculateWeight(double value, double lowerValue, double higherValue)
 {
-    float weight;
-    float difference = higherValue - lowerValue;
+    double weight;
+    double difference = higherValue - lowerValue;
 
     //pc.printf("difference: %f", difference);
     //pc.printf("\n");
 
 
-    weight = (difference - (value - lowerValue))/difference;
+    if(difference == 0)
+    {
+        weight = 1;
+    }else
+    if(difference != 0)
+    {
+        weight = (difference - (value - lowerValue))/difference;
+    }
 
-    //weight = sqrt(weight * weight); //otetaan itsesarvo
 
     //pc.printf("weight: %f", weight);
     //pc.printf("\n");
@@ -214,17 +242,20 @@ float CalculateWeight(float value, float lowerValue, float higherValue)
 void CalculateResistanceMoist()
 {
 
-    UoutMoist =rawDataMoist*Uin; //muutetaan volteiksi. pelkkä raakaData*Uin, jos sarjavastus on plusnavan puolella ja lämpötilasensori miinus puolella.
+    UoutMoist = Uin - rawDataMoist*Uin; //muutetaan volteiksi. pelkkä raakaData*Uin, jos sarjavastus on plusnavan puolella ja lämpötilasensori miinus puolella.
 
-    resistorMoist = resistor2/(Uin/UoutMoist -1);
+    // U = RI -> Rx = Ux/Ix -> Rx = Ux/(U1/R1)
+    resistorMoist = rawDataMoist/((1-rawDataMoist)/resistor2);  //3.3V on supistettu pois ja jännitteen tilalla käytetään raakadataa (1 = 3.3V)
 
-    pc.printf("moisture sensor resistance: %.2f ohm", resistorMoist);
+
+
+    pc.printf("moisture sensor resistance: %f ohm", resistorMoist);
     pc.printf("\n");
 
     //muutetaan kilo ohmeiksi taulukkoa varten
     resistorMoist = resistorMoist/1000;
 
-    pc.printf("moisture sensor resistance: %.2f kOhm", resistorMoist);
+    pc.printf("moisture sensor resistance: %f kOhm", resistorMoist);
     pc.printf("\n");
 
 
@@ -281,8 +312,8 @@ void CalculateVirtualArrayForMoisture()
     {
         virtualArrayForMoisture[i] = moistureArray[i][columLower] * weightTempLower + moistureArray[i][columHigher] * weightTempHigher;
 
-        //pc.printf("virtualArray %d = %.2f", i, virtualArrayForMoisture[i]);
-        //pc.printf("\n");
+        pc.printf("virtualArray %d = %.2f", i, virtualArrayForMoisture[i]);
+        pc.printf("\n");
 
     }
 }
@@ -291,38 +322,61 @@ void CalculateVirtualArrayForMoisture()
 
 void CalculateVirtualRowNumber()
 {
-    rowLower = 14;
-    while (resistorMoist > virtualArrayForMoisture[rowLower]  ) //moistureArray[rowLower][columHigher]
+     vRNcalculated = false;
+
+
+    if(resistorMoist >= virtualArrayForMoisture[0] && vRNcalculated == false)
     {
-        rowLower--;
+        virtualRowNumber = 0;
+        pc.printf("virtual row number pitäs olla 0 mutta on %.f", virtualRowNumber);
+
+        vRNcalculated = true;
+    }//else
+
+    if(resistorMoist <= virtualArrayForMoisture[14] && vRNcalculated == false)
+    {
+        virtualRowNumber = 14;
+        pc.printf("virtual row number pitäs olla 14 mutta on %.f", virtualRowNumber);
+        vRNcalculated = true;
+    }//else
+
+    if(vRNcalculated == false)
+    {
+        rowLower = 14;
+        while (resistorMoist > virtualArrayForMoisture[rowLower]  ) //moistureArray[rowLower][columHigher]
+        {
+            rowLower--;
+        }
+
+        rowHigher = rowLower +1;
+
+       higherResistance  = virtualArrayForMoisture[rowHigher];
+       lowerResistance  = virtualArrayForMoisture[rowLower];
+
+          //pc.printf("ResHi: %.2f", higherResistance);
+          //pc.printf("\n");
+          //pc.printf("ResLo: %.2f", lowerResistance);
+          //pc.printf("\n");
+
+
+       virtualWeightResistanceLow = CalculateWeight(resistorMoist, lowerResistance, higherResistance);
+
+           //pc.printf("virtualWeightLo: %.2f", virtualWeightResistanceLow);
+           //pc.printf("\n");
+
+       virtualWeightResistanceHigh = 1 - virtualWeightResistanceLow;
+
+           //pc.printf("virtualWeightHi: %.2f", virtualWeightResistanceHigh);
+           //pc.printf("\n");
+
+        virtualRowNumber =  rowLower * virtualWeightResistanceLow + rowHigher * virtualWeightResistanceHigh;
     }
 
-    rowHigher = rowLower +1;
+      pc.printf("virtual row number: %.2f", virtualRowNumber);
+      pc.printf("\n");
+      pc.printf("\n");
 
-   higherResistance  = virtualArrayForMoisture[rowHigher];
-   lowerResistance  = virtualArrayForMoisture[rowLower];
-
-      //pc.printf("ResHi: %.2f", higherResistance);
-      //pc.printf("\n");
-      //pc.printf("ResLo: %.2f", lowerResistance);
-      //pc.printf("\n");
-
-
-   virtualWeightResistanceLow = CalculateWeight(resistorMoist, lowerResistance, higherResistance);
-
-       //pc.printf("virtualWeightLo: %.2f", virtualWeightResistanceLow);
-       //pc.printf("\n");
-
-   virtualWeightResistanceHigh = 1 - virtualWeightResistanceLow;
-
-       //pc.printf("virtualWeightHi: %.2f", virtualWeightResistanceHigh);
-       //pc.printf("\n");
-
-    virtualRowNumber =  rowLower * virtualWeightResistanceLow + rowHigher * virtualWeightResistanceHigh;
-
-      //pc.printf("virtual row number: %.2f", virtualRowNumber);
-      //pc.printf("\n");
-      //pc.printf("\n");
+      vRNcalculated = false;
 
 }
 
@@ -342,5 +396,47 @@ void CalculateMoisture()
    //pc.printf("\n");
 
 
+}
+
+void CalculateMoistureAverage()
+{
+    //painotettu keskiarvo
+    moistureAvg1 =  moistureAvg1 * 0.95 + moisture * 0.05;
+
+     //kolmen minuutin keskiarvo
+             moistureAvg2Sum = moistureAvg2Sum - moistureAvg2Array[avgCounter];
+
+            moistureAvg2Array[avgCounter] = moisture;
+             pc.printf("lisättiin paikaan %d", avgCounter);
+             pc.printf("\n");
+
+
+            moistureAvg2Sum = moistureAvg2Sum + moisture;
+
+
+            if(avgCounter <= 16)
+            {
+                 avgCounter++;
+            }else
+
+        if(avgCounter >=17)
+        {
+
+
+
+           //moistureAvg2Sum = moistureAvg2Sum + moisture - moistureAvg2Array[0];
+
+            avgCounter = 0;
+
+           // moistureAvg2Array[avgCounter] = moisture;
+            // pc.printf("lisättiin paikaan %d", avgCounter);
+           //  pc.printf("\n");
+
+
+        }
+
+
+
+            moistureAvg2 = moistureAvg2Sum / 18;
 }
 
